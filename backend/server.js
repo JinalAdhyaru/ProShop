c5649b488671ce162f8c6e01d562f765b9735991
Rakesh Adhyaru <jinal.rakesh-adhyaru@capgemini.com>
2024-03-28 13:35:27 +0530

    Add backend authentication

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -15,10 +17,22 @@ app.get("/", (req, res) => {
     res.send("API is running");
 });
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//cookie parser middleware
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
 app.use("/api/products", productRoutes);
+app.use("/api/user", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => {
     console.log("Server is running on port", port);
-})
+});
