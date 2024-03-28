9f792bc9dda784d8b3dd03abf74cd74473dcda5f
Rakesh Adhyaru <jinal.rakesh-adhyaru@capgemini.com>
2024-03-28 17:59:56 +0530

    Update routes and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,7 +49,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
         generateToken(res, user._id);
 
-        res.send(201).json({
+        res.status(201).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -64,7 +64,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // @desc Logout user and clear cookie
 // @route POST /api/users/logout
-// @access Private
+// @access Public
 const logoutUser = asyncHandler(async (req, res) => {
     res.cookie("jwt", "", {
         httpOnly: true,
@@ -79,7 +79,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
     
-    const user = await User.findById(req,user._id);
+    const user = await User.findById(req.user._id);
 
     if(user) {
         res.status(200).json({
@@ -100,7 +100,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
     
-    const user = await User.findById(req,user._id);
+    const user = await User.findById(req.user._id);
 
     if(user) {
         user.name = req.body.name || user.name;
@@ -116,7 +116,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
             _id: updatedUser._id,
             name: updatedUser.name,
             email: updatedUser.email,
-            isAdmi: updatedUser.isAdmin,
+            isAdmin: updatedUser.isAdmin,
         });
     } else {
         res.status(404);
@@ -129,28 +129,76 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @route GET /api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-    res.send("Get Users");
+
+    const users = await User.find({});
+    res.status(200).json(users);
+
 });
 
 // @desc Get user by ID
 // @route GET /api/users/:id
 // @access Private/Admin
 const getUserByID = asyncHandler(async (req, res) => {
-    res.send("Get User By ID");
+    
+    const user = await User.findById(req.params.id).select('-password');
+    
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+
 });
 
 // @desc Delete users
 // @route DELETE /api/users/:id
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-    res.send("Delete User");
+
+    const user = await User.findById(req.params.id);
+    if (user) {
+        if (user.isAdmin) {
+            res.status(400);
+            throw new Error('Can not delete admin user');
+        }
+        
+        await User.deleteOne({ _id: user._id });
+        res.status(200).json({ message: 'User removed' });
+
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+
 });
 
 // @desc Update users
 // @route PUT /api/users/:id
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-    res.send("Update User");
+
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+        const updatedUser = await user.save();
+        
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        });
+
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+    
 });
 
 export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserByID, updateUser }; 
--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
